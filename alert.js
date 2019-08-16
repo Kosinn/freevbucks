ef0333d429ddb2fb90bbc91d9f5bec03ff8be771
Kosinn <agur.kastemaergmail.com>
2019-08-16 23:38:37 +0300

    added alert stuff

--- alert.js
+++ alert.js
@@ -1,2 +1,12 @@
 alert("uuga buuga nibba nibba");
-alert("I know where you live");
+
+var user = prompt("Please enter your name");
+if (user == "indrek") {
+    alert("furry lol");
+}
+else if (user == "kosin"){
+    alert("Waddap Eesti parim Half Life ja Tekkeni mängija")
+}
+else {
+    alert("Waddap " + user);
+}
