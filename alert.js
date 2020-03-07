9d0d90591251f7f1fc78845b8dd822f79496fc8e
Kosinn <agur.kastemaergmail.com>
2020-03-07 18:11:20 +0200

    removed an alert

--- alert.js
+++ alert.js
@@ -1,12 +1,1 @@
 alert("uuga buuga nibba nibba");
-
-var user = prompt("Please enter your name");
-if (user == "indrek") {
-    alert("furry lol");
-}
-else if (user == "kosin", "Kosin"){
-    alert("Waddap Eesti parim Tekkeni mängija")
-}
-else {
-    alert("Waddap " + user);
-}
