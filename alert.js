fa2772b15416a73711046fd40a1947bb8bfd811d
Kosinn <agur.kastemaergmail.com>
2020-03-07 17:48:18 +0200

    changed gif

--- alert.js
+++ alert.js
@@ -4,8 +4,8 @@ var user = prompt("Please enter your name");
 if (user == "indrek") {
     alert("furry lol");
 }
-else if (user == "kosin"){
-    alert("Waddap Eesti parim Half Life ja Tekkeni mängija")
+else if (user == "kosin", "Kosin"){
+    alert("Waddap Eesti parim Tekkeni mängija")
 }
 else {
     alert("Waddap " + user);
